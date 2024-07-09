dcb988e0026be70c8dc1a276bb1137746a75d709
Vrush2004 <vrushalichaube@gmail.com>
2024-07-09 18:11:28 +0530

    skill section added

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ var typed = new Typed(".text", {
     loop:true
 });
 
+const circles = document.querySelectorAll('.circle')
+circles.forEach(elem=>{
+    var dots = parseInt(elem.getAttribute("data-dots"));
+    var marked = parseInt(elem.getAttribute("data-percent"));
+    var percent = Math.floor(dots*marked/100);
+    var rotate = 360 / dots
+    let points = "";
+    for(let i = 0; i<dots; i++){
+        points += `<div class='points' style='--i:${i}; --rot:${rotate}deg'></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i=0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
